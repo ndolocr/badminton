3fcc102de90d91aa9f132aa3b0667c772317ef7f
ndolocr <crndolo@gmail.com>
2022-12-27 17:36:12 +0300

    including gender choices for trainee

--- trainee/models.py
+++ trainee/models.py
@@ -8,9 +8,16 @@ class Trainee(models.Model):
         ("beginner", "beginner"),
         ("intermediate", "intermediate")
     )
+
+    GENDER_CHOICES = (
+        ("male", "male"),
+        ("female", "female"),
+        ("other", "other")
+    )
+
     name = models.CharField(max_length=255, null=True)
-    gender = models.CharField(max_length=20, null=True)
     dob = models.DateField(auto_now=False, null=True)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, null=True)
     trailer_level = models.CharField(max_length=30, choices=TRAINEE_LEVEL_CHOICES, null=False)
 
     def __str__(self):
