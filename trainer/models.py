55bd913d6b7f90dc2d18756f49a0671f0207c733
ndolocr <crndolo@gmail.com>
2022-12-27 16:44:16 +0300

    Creating a model for trainer

--- trainer/models.py
+++ trainer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Trainer(models.Model):
+    TRAINER_LEVEL_CHOICES = (
+        ("expert", "expert"),
+        ("beginner", "beginner"),
+        ("intermediate", "intermediate")
+    )
+    name = models.CharField(max_length=255, null=True)
+    gender = models.CharField(max_length=20, null=True)
+    trailer_level = models.CharField(max_length=30, choices=TRAINER_LEVEL_CHOICES, null=False)
+
+    def __str__(self):
+        return self.name
+    
