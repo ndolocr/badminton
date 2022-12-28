bbd9454e2f53b523549133a1945bdd5598afd0bb
ndolocr <crndolo@gmail.com>
2022-12-28 23:16:47 +0300

    Include html templates for displaying graph and creating a view to load the graph

--- badminton/settings.py
+++ badminton/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'badminton.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'badminton.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -129,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- core/views.py
+++ core/views.py
@@ -1,11 +1,31 @@
+import os
 from django.conf import settings
 from pyvis.network import Network
 from django.shortcuts import render
+from training.models import Training
 
 # Create your views here.
 
 def show_pyvis_graph(request):
     network = Network(height="100vh", width="100%", bgcolor="#eeeeee" )
-    network.save_graph(str(settings.BASE_DIR)+'/negative_data/templates/negative_data/pyvis_graph.html')
+    
+    try:
+        training = Training.objects.all()
+        print("All Trainings: -->", training)
+
+        if training:
+            for training_set in training:
+                print("Trainer: --->", training_set.trainer)
+                trainer = network.add_node(training_set.trainer.name, title='Trainer', color="#FF0")
+                for trainee_set in training:
+                    if trainee_set.trainer == training_set.trainer:
+                        print("Trainee: --->", trainee_set.trainee)
+                        trainee = network.add_node(trainee_set.trainee.name, title='Trainee')
+                        training_link = network.add_edge(training_set.trainer.name, trainee_set.trainee.name, title='Trains')
+                        print("Edge: -->", training_link)
+        network.save_graph(str(settings.BASE_DIR)+'/templates/pyvis_graph.html')
+    except Exception as e:
+        print(e)
+
     context = {}
-    return render(request, "core/graph.html", context)
+    return render(request, "graph.html", context)
