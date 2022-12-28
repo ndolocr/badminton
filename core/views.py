cc08ef9749c7b1e74a144062a53f420d855e2525
ndolocr <crndolo@gmail.com>
2022-12-28 20:33:18 +0300

    Creating function to view graph

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.conf import settings
 from pyvis.network import Network
+from django.shortcuts import render
 
 # Create your views here.
+
+def show_pyvis_graph(request):
+    network = Network(height="100vh", width="100%", bgcolor="#eeeeee" )
+    network.save_graph(str(settings.BASE_DIR)+'/negative_data/templates/negative_data/pyvis_graph.html')
+    context = {}
+    return render(request, "core/graph.html", context)
