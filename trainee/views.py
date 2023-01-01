ea1d4504039f2e08891f6d08bd80fbc410d1054c
ndolocr <crndolo@gmail.com>
2023-01-01 17:40:33 +0300

    Creating view to load trainee landing page

--- trainee/views.py
+++ trainee/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from trainee.models import Trainee
 # Create your views here.
+
+def index(request):
+    trainees = Trainee.objects.all()
+    context = {"trainees": trainees}
+    return render(request, 'trainee/index.html', context)
