e34674531baf0cc3643cf6874501e55e2dedcbb8
ndolocr <crndolo@gmail.com>
2023-01-03 09:21:03 +0300

    Creating urls for the different trianing types

--- training/urls.py
+++ training/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from training.views import index
+from training.views import index, expert_training, beginner_training, intermediate_training
 
 app_name = 'training'
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('/expert/training', expert_training, name='expert'),
+    path('/beginner/training', beginner_training, name='beginner'),
+    path('intermediate/training', intermediate_training, name='intermediate'),
 ]
